15176253f02fa65db655828bc5a838c4ceccb824
OhWhale515 <sterlinggreen515@gmail.com>
2023-09-17 14:12:25 -0400

    start

---
+++ todo.py
@@ -0,0 +1,10 @@
+# Import any necessary libraries here
+from tkinter import *
+
+# Define global variables (if needed)
+
+# Define functions for each feature (e.g., add_task, list_tasks, etc.)
+
+# Implement the main program loop
+if __name__ == "__main__":
+    # Your code for user interaction and menu options goes here.
